fe816ca0929a9e32d37131d66691ccfe5c6bd7ba
Dereje Jima <ddjima2014@gmail.com>
2023-06-06 13:07:01 -0400

    inital commit

---
+++ R/load_idata.R
@@ -0,0 +1,15 @@
+
+#' load_idata
+#'
+#' @param idata load the idata from red and green channel
+#'
+#' @return a matrix samples to load
+#' @export
+#'
+#' @examples
+#' load-data(input_data)
+load_idata <- function(idata) {
+  # write this later
+  warning("You ran foo and I haven't written it yet")
+
+}
