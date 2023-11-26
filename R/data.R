163954e5be86764252882b4f86e97213868e0219
Bruce Corliss <bacorli2@ncsu.edu>
2023-11-25 19:54:59 -0500

    Started migrating basic pipeline into package. Added array imprintome manifest as a rda data file.

---
+++ R/data.R
@@ -0,0 +1,50 @@
+
+
+
+#' True Diagnostic Imprintome Manifest File
+#'
+#' A metadata file that maps illumina probes used in the imprintome array to CpG
+#' sites in the human genome
+#'
+#' @format A data frame with 22819 rows and 23 columns
+#' \describe{
+#'   \item{Probe_ID}{   }
+#'   \item{Name}{   }
+#'   \item{U}{  }
+#'   \item{AlleleA_ProbeSeq}{  }
+#'   \item{M}{  }
+#'   \item{AlleleB_ProbeSeq}{  }
+#'   \item{Next_Base}{  }
+#'   \item{COLOR_CHANNEL}{  }
+#'   \item{col}{  }
+#'   \item{Probe_Type}{  }
+#'   \item{Strand_FR}{  }
+#'   \item{Strand_TB}{  }
+#'   \item{Strand_CO}{  }
+#'   \item{Infinium_Design}{  }
+#'   \item{Infinium_Design_Type}{  }
+#'   \item{CHR}{  }
+#'   \item{MAPINFO}{  }
+#'   \item{Species}{  }
+#'   \item{Genome_Build}{  }
+#'   \item{Source_Seq}{  }
+#'   \item{Forward_Sequence}{  }
+#'   \item{Top_Sequence}{  }
+#'   \item{Rep_Num}{  }
+#' }
+#' @source True Diagnostic
+"manifest_v1A2"
+
+
+#' Sample data
+#'
+#' data to be filled in
+#'
+#' @format none
+#' \describe{
+#'   \item{Null}{   }
+#' }
+#' @source none
+"sample_data"
+
+
--- R/load_idata.R
+++ R/load_idata.R
@@ -7,37 +7,89 @@
 #' or via the \code{\link{Summary}} group generic. For this to work properly,
 #' the arguments \code{...} should be unnamed, and dispatch is on the
 #' first argument.
-#' @param idata load the idata from red and green channel
+#' @param idat_dir_paths the full filesystem path to directory containing the idat
+#' files to be processed, formatted as a string or a vector of strings.
+#' @param mft manifest file for particular imprintome array used in data collection.
+#' A dataframe with metadata mapping probe_ids to CpG sites. See ?manifest_v1A2 dataset.
+#' @param multicore boolean flag for whether multicore processing should be used
+#'  when importing IDAT files.
+#' @importFrom sesame openSesame
+#' @importFrom stringr str_replace regex
 #'
-#' @return a matrix samples to load
+#'
+#' @returns a matrix samples to load
 #' @export
 #'
-#' @examples
-#' load-data(input_data)
-load_idata <- function(idata) {
-    # write this later
-    warning("You ran foo and I haven't written it yet")
-
-}
+#'
+load_idata <- function(idat_dir_paths, mft = NULL, multicore = TRUE) {
 
 
-library(ggplot2)
-show_mtcars <- function() {
-  qplot(mpg, wt, data = mtcars)
-}
+  # Load manifest file
+  mft = tdhia::manifest_v1A2
+  print(mft)
 
-# You need the suggested package for this function
-my_fun1 <- function(a, b) {
-  if (!requireNamespace("pkg", quietly = TRUE)) {
-    stop("Pkg needed for this function to work. Please install it.",
-         call. = FALSE)
+  # Get list of IDAT files in target path
+  all_idat_names <- dir(path = idat_dir_paths, pattern = "*.idat", ignore.case = TRUE)
+  # Extract the basenames of IDAT files
+  #   Removes _Grn.idat and _Red.idat from file names (case insensitive)
+  all_idat_basenames <- stringr::str_replace(
+    all_idat_names, pattern = stringr::regex("_(Grn|Red).idat$", ignore_case = TRUE),
+    replacement = "")
+
+
+  # Debugging: verify that two IDAT files are associated with each basename:
+  #    One file for red and one for green channel fluorescence
+  basename_tbl <- as.data.frame(table(all_idat_basenames))
+  if (!all(basename_tbl$Freq==2)){
+    warning(paste("IDAT_PAIR: Not all IDAT file basenames have 2 IDAT files associated with them.",
+                  "Files are missing! See basename_tbl variable"))
   }
+
+  # Get unique list of basenames (removes repeats)
+  unq_idat_basenames <- unique(all_idat_basenames)
+  #
+  # # Load study meta_data
+  # # study metadata file needs to have these columns
+  # #   source: name of study that data originated from
+  # #   patient_study_id: an integer, study-specific simple ID # for each patient
+  # #   Patient_ID: the original medical record number/code for patient
+  # #
+  # # Note: the same patient participating in different studies will have different
+  # #   Patient_IDs!
+  # #
+  # # Even though Patient_ID and patient_study_id have similar roles, its better to
+  # #   Use a simple integer index for downstream processing.
+  # # study_meta <- read_excel(study_meta_path)
+  # # study_meta$patient_global_id <- paste(study_meta$source, study_meta$patient_study_id, sep = "_")
+  #
+  # # # List any patient IDs not found in IDAT files
+  # # target_IDAT_basenames <- intersect(study_meta$Patient_ID, unq_idat_basenames)
+  # # missing_idats <- setdiff(study_meta$Patient_ID, unq_idat_basenames)
+  # # if (!length(missing_idats)==0) {
+  # #   warning("There are missing IDAT files for some patients in study metadata, see 'missing_idats' variable")
+  # # }
+  #
+  #
+  #
+  # # Processing IDAT files into beta matrices
+  # ################################################################################
+  # # Convert florescence data from array into beta values
+  #
+  # # Set Multicore parameter depending on operating system
+  # if (.Platform$OS.type == "windows" && multicore) {
+  #   multicore_arg <- BiocParallel::SnowParam(max(c(parallel::detectCores()-1,1)))
+  # } else if (multicore) {
+  #   multicore_arg <- BiocParallel::MulticoreParam(max(c(parallel::detectCores()-1,1)))
+  # } else {
+  #   multicore_arg = NULL
+  # }
+  #
+  # # Load each of the IDAT file pairs, process with standard SeSame pipeline
+  # probe_beta_matrix <- openSesame(paste0(idat_dir_paths, '/', target_IDAT_basenames),
+  #                                 platform="TruDx_imprintome", manifest = mft, mask=F,
+  #                                 BPPARAM = multicore_arg)
+  #
+  # return(probe_beta_matrix)
+
 }
-# There's a fallback method if the package isn't available
-my_fun2 <- function(a, b) {
-  if (requireNamespace("pkg", quietly = TRUE)) {
-    pkg::f()
-  } else {
-    g()
-  }
-}
+
