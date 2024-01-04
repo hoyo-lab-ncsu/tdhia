200e7cd05dd4de1279399f8b8a03248a4e69fc80
bacorliss <44813189+bacorliss@users.noreply.github.com>
2024-01-03 21:34:02 -0500

    Bugfix: probes are now filtered specifically those that map to a CpG cite with cg* regexp in probe_id.

--- R/filter_probes.R
+++ R/filter_probes.R
@@ -8,15 +8,19 @@
 #'   $probe_beta_df: a dataframe of beta values, probe_id x sample_id
 #'   $probe_pval_df: a dataframe of signal detection p-values, probe_id x sample_id
 #' @param discard_unmapped_probes boolean flag, when TRUE probes that do not map
-#' to a unique genomic position are discarded from the probe beta and
-#' p-value dataframes.
+#' to a unique genomic position or CpG site are discarded from the probe beta and
+#' p-value dataframes. Probes that match to a CpG site are identified by their prefix
+#' in the probe name (<prefix><index>_<version tag>). Prefixes are (ctl:control,
+#' cg:CpG, ch:CHG, mu:multi-unique, rp:repetitive element, or rs:SNP)
 #' @param max_sig_pval a maximum threshold for the signal detection value, beta
 #' values higher than this max are set to NA.
 #' @param max_probe_fail_rate proportion value (0-1), if a probe fails the p-value
 #' threshold above this proportion of the samples, all measurements from this probe
 #' are set to NA.
 #' @param discard_failed_probes boolean flag, when TRUE any probes that have NA
-#' values across all samples are discarded.
+#' values across all samples are discarded (default = TRUE)
+#' @param verbose boolean flag, when TRUE prints the results of each filtering
+#' step (default = TRUE).
 #'
 #' @returns a dataframe containing beta values where rows are probe_ids and columns
 #' are the sample_ids (basenames of the IDAT files or some other mapping
@@ -24,16 +28,20 @@
 #'
 filter_probes <- function(probe_beta, discard_unmapped_probes = TRUE,
                           max_sig_pval = 0.2, max_probe_fail_rate = 0.5,
-                          discard_failed_probes = TRUE) {
+                          discard_failed_probes = TRUE, verbose = TRUE) {
   # save(list = ls(all.names = TRUE), file = "filter_probes_debug.RData")
   # load(file = "filter_probes_debug.RData")
 
+  # Only print cat() output if the user requests it
+  verbosecat <-function(x) if (verbose) cat(x)
+
   # Extract manifest dataframe
   mft = probe_beta$manifest
-  cat(sprintf("Probe manifest: manifest file has a total of %.0f probes.\n",
+  verbosecat(sprintf("Probe manifest: manifest file has a total of %.0f probes.\n",
               nrow(mft)))
 
-  # Discard probe_ids(s) that do not map uniquely to a single genome location
+
+  # Discard probe_ids(s) that do not map uniquely to genomic location and CpG
   #----------------------------------------------------------------------------
   #   (Defined in manifest file where MAPINFO == (0 or NA))
   #   Data is copied over to filter_* probe_beta and probe_ pval matrices.
@@ -41,15 +49,25 @@ filter_probes <- function(probe_beta, discard_unmapped_probes = TRUE,
     f_unmmaped = function (x) is.na(x) | x==0;
     unmapped_probe_ids <- sapply(mft$Probe_ID,
                                  function(x) f_unmmaped(mft$MAPINFO[which(x==mft$Probe_ID)[1]]))
-    filt_probe_beta_df <- probe_beta$probe_beta_df[!unmapped_probe_ids,]
-    filt_probe_pval_df <- probe_beta$probe_pval_df[!unmapped_probe_ids,]
 
-    cat(sprintf("Probe filter: discarding %.0f%% probes ( %i/ %i) in dataset  b/c they don't
-                map uniquely to the genome. %.0f probes now remain.\n",
+    # Keep probes that target a cg site
+    cpg_ids <- grepl("^cg",rownames(probe_beta$probe_beta_df))
+    # For reference probe_id stars with either:
+    # ctl: control, cg: CPG, ch: CHG, mu: multi-unique,
+    # rp: repetitive element, or rs: SNP probe
+
+    # Keep probes that are not unmapped and are a cpg site
+    filt_probe_beta_df <- probe_beta$probe_beta_df[!unmapped_probe_ids & cpg_ids,]
+    filt_probe_pval_df <- probe_beta$probe_pval_df[!unmapped_probe_ids & cpg_ids,]
+
+    verbosecat(sprintf("Probe filter: discarding %.0f%% probes ( %i/ %i) in dataset  b/c they don't
+                map uniquely to the genome or a CpG site.
+                %.0f probes now remain.\n",
                 100*nrow(filt_probe_beta_df)/nrow(probe_beta$probe_beta_df),
                 nrow(probe_beta$probe_beta_df)- nrow(filt_probe_beta_df),
                 nrow(probe_beta$probe_beta_df),
                 nrow(filt_probe_beta_df)))
+
   } else {
     # Copy the probe_beta and p-value matrices and beginning filtering steps
     filt_probe_beta_df <- probe_beta$probe_beta_df
@@ -61,7 +79,7 @@ filter_probes <- function(probe_beta, discard_unmapped_probes = TRUE,
   #-----------------------------------------------------------------------------
   if (!is.null(max_sig_pval)) {
     filt_probe_beta_df[filt_probe_pval_df > max_sig_pval] <- NA
-    cat(sprintf("Probe filter: %.0f%% of probe beta measurements ( %i/ %i) failed
+    verbosecat(sprintf("Probe filter: %.0f%% of probe beta measurements ( %i/ %i) failed
                  the signal max p-value threshold of %.2f, setting them to NA.\n",
                 100*sum(is.na(filt_probe_beta_df))/prod(dim(filt_probe_beta_df)),
                 sum(is.na(filt_probe_beta_df)),prod(dim(filt_probe_beta_df)),
@@ -76,14 +94,14 @@ filter_probes <- function(probe_beta, discard_unmapped_probes = TRUE,
   #-----------------------------------------------------------------------------
   if(!is.null(max_probe_fail_rate)) {
     # Binary index of rows, TRUE means row failed QC
-    probe_fail_rate_df <- data.frame(cpg_id <- rownames(filt_probe_beta_df),
+    probe_fail_rate_df <- data.frame(cpg_id = rownames(filt_probe_beta_df),
                                 probe_fail_rate = rowSums(is.na(filt_probe_beta_df)) /
                                   ncol(filt_probe_beta_df))
 
     row_fail_flag <-  probe_fail_rate_df$probe_fail_rate > max_probe_fail_rate
     # Set rows to NA
     filt_probe_beta_df[row_fail_flag,] <- NA
-    cat(sprintf("Probe filter: %.0f%% of probes ( %i/ %i) had a signal p-value
+    verbosecat(sprintf("Probe filter: %.0f%% of probes ( %i/ %i) had a signal p-value
                 fail rate above threshold of %.0f%%, setting all beta values to NA
                 for those probes.\n",
         100*sum(row_fail_flag)/length(row_fail_flag), sum(row_fail_flag),
@@ -106,7 +124,7 @@ filter_probes <- function(probe_beta, discard_unmapped_probes = TRUE,
     filt_probe_beta_df2 <- filt_probe_beta_df
     filt_probe_pval_df2 <- filt_probe_pval_df
   }
-  cat(sprintf("Probe filter: discarded %.0f%% probes ( %i/ %i) because all measurements are now NA.
+  verbosecat(sprintf("Probe filter: discarded %.0f%% probes ( %i/ %i) because all measurements are now NA.
               %i probes now remain. \n",
       100*(nrow(filt_probe_beta_df) - nrow(filt_probe_beta_df2))/nrow(filt_probe_beta_df),
       nrow(filt_probe_beta_df) - nrow(filt_probe_beta_df2),
