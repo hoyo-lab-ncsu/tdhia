0f2fce5d60672ec834603afbb8056d14b5b30c81
bacorliss <44813189+bacorliss@users.noreply.github.com>
2024-01-19 10:46:14 -0500

    Renamed analyze_imprintome_study -> study_imprint, and simplified inputs.

--- R/study_imprint.R
+++ R/study_imprint.R
@@ -1,73 +1,62 @@
 
 
-#' analyze_imprintome_study
+#' study_imprint
 #'
-#' @description performs a statistical analysis with CpG sites whose methylation
-#' state are correlated with a response variable while also considering several
-#' covariates. CpG methylation status/ beta values are used as predictors, from
-#' the study metadata a response variable is chosen along with any co-factors. The
-#' presence of an association is tested for each of the predictor variables to
-#' the response variable.
+#' @description fits a series of general linear models with parallel processing
+#' using response variable(s) R, predictor variable(s) R and confouding
+#' variable(s) C. The model formula takes on the form:
 #'
-#' @param model_params a named list that defines parameter for the model to be
-#' fitted to the data, with the following fields:
-#'  - R: response variable(s), data frame that is patients (rows) x variables
+#' If R has many variables/ columns and P has one, models are parallelized over R:
+#'
+#' R\[,i] ~ P\[,1] + C\[,1] + C\[,2] + C\[,3] ...
+#'
+#' If P has many variables/ columns and R has one, models are parallelized over P:
+#'
+#' R\[,1] ~ P\[,i] + C\[,1] + C\[,2] + C\[,3] ...
+#'
+#' This designed that either R or P are the CpG beta values or the ICR beta
+#' values. The code handles other cases.
+#'
+#' @param R response variable(s): data frame that is patients (rows) x variables
 #'  (columns) could either be beta values for cpg or ICR sites, or
 #'  study metadata.
-#'  - Rind: single value or array that represents column index for which variables
-#'  in R to use in the model, for the case of multiple columns, will be parallelized
-#'  in separate models. Only Rind or Pind can have more than one value, but not
-#'  both (defaults to 1).
-#'  - P: predictor variable(s) data frame that is patients (rows) x variables
+#' @param P predictor variable(s) data frame that is patients (rows) x variables
 #'  (columns), could either be beta values for cpg or ICR sites, or
 #'  study metadata.
-#'  - Pind: single value or array that represents column index for which variables
-#'  in P to use in the model, for the case of multiple columns, will be parallelized
-#'  in separate models. Only Rind or Pind can have more than one value, but not
-#'  both (defaults to 1).
-#'  - C: confounder variables data frame that is patients (rows) x variables
+#' @param C predictor variable(s) data frame that is patients (rows) x variables
 #'  (columns).
-#'  - family: string for family model argument for glm(). (ex. 'binomial', 'gaussian')
-#'  - n_p_adj: n for p value adjustment
-#'
-#'  @param max_p_val max p-value threshold, prints number of entries below this
+#' @param family string for family model argument for glm(). (ex. 'binomial', 'gaussian')
+#' @param n_p_adj number of comparisons to use for p-value adjustment. Sometimes
+#'  is this is different than the number of actual comparisons (could be more or
+#'  less). Defaults to max columns to response and predictor variables
+#' @param max_p_val max p-value threshold, prints number of entries below this
 #'  threshold, but does not alter computation.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom foreach %dopar%
 #' @importFrom rlang .data
 #'
-#' @return sorted results from statistical analysis
-#'
-analyze_imprintome_study <- function (model_params, max_p_val = 0.05) {
+#' @return results with fitted coefficients from the glm, sorted by p-value
+study_imprint <- function (R, P, C, family, n_p_adj = max(c(ncol(R), ncol(P))),
+                                      max_p_val = 0.05) {
 
   save(list = ls(all.names = TRUE), file = "analyze_imprintome_study.RData")
   # load(file = "analyze_imprintome_study.RData")
-
-  # Export model parameters while keeping both as dataframes even if 1 column
-  # Response variable(s)
-  R <- model_params[["R"]]
-  # Predictor variable(s)
-  P <- model_params[["P"]]
-  # Confounder Variable(s)
-  C <- model_params[["C"]]
-  family <- model_params$family
-  n_p_adj <- model_params$n_p_adj
 
   # Reorder rows in P and C to match R, keep both as dataframes even if 1 column
   P <- P %>%
     tibble::rownames_to_column(var = "row_names") %>%
     dplyr::arrange(factor(.data$row_names, levels = rownames(R))) %>%
     tibble::column_to_rownames(var = "row_names")
+  if( !all(rownames(R)==rownames(P))) {
+    stop("glm: rownames of R and P dataframes do not match.")}
+
   C <- C %>%
     tibble::rownames_to_column(var = "row_names") %>%
     dplyr::arrange(factor(.data$row_names, levels = rownames(R))) %>%
     tibble::column_to_rownames(var = "row_names")
-
-  if( !all(rownames(R)==rownames(P)))
-  {stop("glm: rownames of R and P dataframes do not match.")}
-  if( !all(rownames(R)==rownames(C)))
-  {stop("glm: rownames of R and C dataframes do not match.")}
+  if( !all(rownames(R)==rownames(C))) {
+    stop("glm: rownames of R and C dataframes do not match.")}
 
 
   #  Get indexes for response and predictor variables
@@ -101,7 +90,7 @@ analyze_imprintome_study <- function (model_params, max_p_val = 0.05) {
   cat(sprintf("Formula: %s\n", test_run$Formula))
 
 
-  #Initialize parallel computing
+  # Initialize parallel computing
   cl <- parallel::makePSOCKcluster(parallel::detectCores() - 1)
   doParallel::registerDoParallel(cl)
 
