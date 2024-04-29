0066d589db08c3824a334a971d9505bffbf62f09
bacorli2 <150067594+bacorli2@users.noreply.github.com>
2024-04-29 15:26:44 -0400

    Added function to rename idats to capitalization pattern supported by SeSame. Doc updates.

---
+++ R/correct_idat_names.R
@@ -0,0 +1,35 @@
+
+#' correct_idat_names
+#'
+#' @description correct capialtization to match the convention expected by the
+#' sesame package. Example filename: 4207113116_A_Grn.idat.
+#'
+#' @param idat_dir_path full filesystem path to location of idats to be renamed.
+#'
+#' @return results with fitted coefficients from the glm, sorted by p-value
+correct_idat_names <- function (idat_dir_path) {
+
+  # Get all idat files in target directory
+  idat_fullnames <- dir(path = idat_dir_path, pattern = "/*.idat",
+                            full.names = TRUE, ignore.case = TRUE)
+
+  # For IDAT extention lower case
+  new_idat_fullnames = stringr::str_replace(idat_fullnames, '.IDAT$', '.idat')
+  # Force green camel case
+  new_idat_fullnames = stringr::str_replace(new_idat_fullnames, 'grn.idat', 'Grn.idat')
+  # Force red camel case
+  new_idat_fullnames = stringr::str_replace(new_idat_fullnames, 'red.idat', 'Red.idat')
+
+  # organize into dataframe
+  df_log = data.frame(old_name = idat_fullnames, new_name = new_idat_fullnames)
+
+  # Write log of proposed renames
+  write.csv(x=df_log, file = paste0(idat_dir_path, '/idat_rename_log.csv'))
+
+  # Rename files
+  file.rename(from = df_log$old_name, to = df_log$new_name)
+
+}
+
+
+
