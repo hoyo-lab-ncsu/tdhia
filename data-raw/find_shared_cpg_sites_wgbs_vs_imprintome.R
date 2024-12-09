6ffd531d46addf02f8877309741bb14429179a3b
bacorliss <44813189+bacorliss@users.noreply.github.com>
2024-12-09 14:20:17 -0500

    First draft of shared wgbs script complete, need to convert to patient/ study ids.

--- data-raw/find_shared_cpg_sites_wgbs_vs_imprintome.R
+++ data-raw/find_shared_cpg_sites_wgbs_vs_imprintome.R
@@ -2,7 +2,7 @@
 
 
 here::i_am("README.md")
-
+devtools::load_all()
 
 # Load each patient file, filter for overlap between manifest and wgbs
 
@@ -17,34 +17,40 @@ bed_list <- list.files(path = data_dir, pattern = ".bed$")
 mft <- tdhia::manifest_v1A2_design_scores
 cpg_atlas <- tdhia::mapping_cpg_icr_ids
 cpg_atlas$chr_cpg_start = paste0(cpg_atlas$CpG_chr, "::", cpg_atlas$CpG_start)
+unq_cpg_atlas <- dplyr::distinct(dplyr::select(cpg_atlas, -c(CpG_Probe,X)))
+
+# The WGBS data has unique sites with every file
+# Therefore the stored data from each patient should be based on the sites in the
+#  imprintome manifest, and not based on the sites from each patient.
+
 
 df_ratio_list = list()
 for (n in seq_along(bed_list)) {
-cat(sprintf("Processing file %.0f/%.0f\n", n, length(bed_list)))
-# Read in BED file for WGBS data
-df_wgbs <- as.data.frame(data.table::fread(paste0(data_dir, "/", bed_list[n])))
-df_wgbs$chr_cpg_start = paste0("chr", df_wgbs[["#chrom"]], "::", df_wgbs$start)
+  cat(sprintf("Processing file %.0f/%.0f\n", n, length(bed_list)))
+  # Read in BED file for WGBS data
+  df_wgbs <- as.data.frame(data.table::fread(paste0(data_dir, "/", bed_list[n])))
+  df_wgbs$chr_cpg_start = paste0("chr", df_wgbs[["#chrom"]], "::", df_wgbs$start)
 
 
-# Filter rows of WGBS that match cpg sites in atlas
-df_wgbs$in_imprintome <- df_wgbs$chr_cpg_start  %in% cpg_atlas$chr_cpg_start
-sub_df_wgbs <- df_wgbs[df_wgbs$in_imprintome,]
+  # Filter rows of WGBS that match cpg sites in atlas
+  df_wgbs$in_imprintome <- df_wgbs$chr_cpg_start  %in% unq_cpg_atlas$chr_cpg_start
+  sub_df_wgbs <- df_wgbs[df_wgbs$in_imprintome,]
 
 
-# dup_flag <- duplicated(select(sub_df_wgbs, "chr_cpg_start", "start", "end", "ratio"))
-# Add cpg_id to thhe wgbs results table
-merged_df_wgbs <-
-  left_join(x = dplyr::select(sub_df_wgbs, "chr_cpg_start", "start", "end", "ratio"),
-          y = dplyr::select(cpg_atlas, c("chr_cpg_start", "CpG_id")), by = "chr_cpg_start",
-           keep = FALSE, multiple = "first")
+  # Add cpg_id to unq_cpg_atlast dataframe so that cpg side have same order and
+  #  IDs across patients
+  merged_df_wgbs <-
+    dplyr::left_join(x = dplyr::select(unq_cpg_atlas, c("chr_cpg_start", "CpG_id")),
+              y = dplyr::select(sub_df_wgbs, "chr_cpg_start", "start", "end", "ratio"), by = "chr_cpg_start",
+              keep = FALSE, multiple = "first")
 
 
-# Extract the ratio data into a data.frame column (with rownames as cpg_id)
-df_ratio_list[[n]] <- data.frame("ratio"= merged_df_wgbs$ratio, row.names = merged_df_wgbs$CpG_id)
+  # Extract the ratio data into a data.frame column (with rownames as cpg_id)
+  df_ratio_list[[n]] <- data.frame("ratio"= merged_df_wgbs$ratio, row.names = merged_df_wgbs$CpG_id)
 
 }
 
 
-
+df_ratio <- do.call(cbind,df_ratio_list)
 
 
