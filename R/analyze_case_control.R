d7cfa7784c32ae6da8dfa0455822ef4adab56297
bacorliss <44813189+bacorliss@users.noreply.github.com>
2025-04-09 20:37:14 -0400

    Extended stats collected for case_control analysis.

--- R/analyze_case_control.R
+++ R/analyze_case_control.R
@@ -11,10 +11,10 @@
 #' @param n_adjust number to adjust p-values for FDR multiple comparisons
 #'
 #' @export
-analyze_case_control <- function(beta_matrix, ctrl_cols, n_adjust = nrow(beta_matrix)) {
-  #Initialize a vector of NAs to store p-values
-  df <- data.frame(id = 1:nrow(beta_matrix), imp_id = NA, p_vals = rep(NA, nrow(beta_matrix)),
-                   mean1 = NA, mean2 = NA, std1= NA, std2 = NA)
+analyze_case_control <- function(beta_matrix, ctrl_cols, n_adjust = nrow(beta_matrix), filter_na_pval = TRUE) {
+  # Initialize a vector of NAs to store p-values
+  df <- data.frame(id = 1:nrow(beta_matrix), imp_id = NA, p_val = rep(NA, nrow(beta_matrix)),
+                   adj_pval = NA, adj_sig = NA, mean1 = NA, mean2 = NA, std1= NA, std2 = NA)
   
 
   #Loop through each row of beta_matrix
@@ -24,10 +24,15 @@ analyze_case_control <- function(beta_matrix, ctrl_cols, n_adjust = nrow(beta_ma
     group2<- as.numeric(beta_matrix[i, !ctrl_cols])
 
     #Perform two-sample t-test
-    t_test_result<- stats::t.test(group1,group2)
+    if ((length(group1[!is.na(group1)]) > 2) & 
+        (length(group2[!is.na(group2)]) > 2)   ) {
+      t_test_result<- stats::t.test(group1,group2)
+    } else {
+      t_test_result = list(p.value = NA)
+    }
 
     #Store the p-value in P_val
-    df$p_vals[i] <- t_test_result$p.value
+    df$p_val[i] <- t_test_result$p.value
     df$mean1[i] <- mean(group1, na.rm = TRUE)
     df$mean2[i] <- mean(group2, na.rm = TRUE)
     df$std1[i] <-  sd(group1, na.rm = TRUE)
@@ -36,14 +41,17 @@ analyze_case_control <- function(beta_matrix, ctrl_cols, n_adjust = nrow(beta_ma
     df$frac_na2[i] <-  sum(is.na(group2))/length(group2)
   }
 
-
-  # Adjust p-values based on number of ICRs (more liberal adjustment- this one has hits )
-  df$adj_pval <- custom_p.adjust(df$p_vals, method = "fdr", n = n_adjust)
-  df$adj_sig <- (df$adj_pval < 0.05)
-  df$imp_id <- rownames(beta_matrix)
   df$delta <- df$mean2 - df$mean1
   df$perc_delta <- 100*(df$mean2 - df$mean1) / df$mean1
-  # out <- data.frame(id = rownames(beta_matrix), p_val = p_vals,
-  #                   adj_pval = adj_pvals, adj_sig = adj_sigs)
+  df$imp_id <- rownames(beta_matrix)
+  
+  # Remove rows where a p-value can't be calculated (required for p-value adjustment)
+  if (filter_na_pval) {    df <- df[!is.na(df$p_val),]    }
+  # Adjust p-values based on number of ICRs (more liberal adjustment- this one has hits )
+  df$adj_pval <- custom_p.adjust(df$p_val, method = "fdr", n = n_adjust)
+  df$adj_sig <- (df$adj_pval < 0.05)
+  
+
+  
   return(df)
 }
