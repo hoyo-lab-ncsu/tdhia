90cd846ac65bc3815f6bb6e92204d193b30e873a
bacorliss <44813189+bacorliss@users.noreply.github.com>
2025-04-28 21:43:40 -0400

    Added input args to cpg-icr methylation dot plots.

--- R/plot_icr_methylation.R
+++ R/plot_icr_methylation.R
@@ -14,8 +14,11 @@
 #' @returns a ggplot function handle to the plot.
 #'
 #' @export
-plot_icr_methylation <- function(mat_cpg_beta, df_patient_groups, icr_id, 
-                                 plot_height_width = c(5,3)) {
+plot_icr_methylation <- function(mat_cpg_beta, sig_cpgs = NA, df_patient_groups, icr_id, xlab_txt = "", 
+                                 plot_height_width = c(5,3), output_path, max_sig_hwindow = 9) {
+  
+  dir.create(path = output_path, recursive = TRUE, showWarnings = FALSE)
+  
   
   # Get list of CpGs for specified icr
   df = left_join(x = data.frame(cpg_id = rownames(mat_cpg_beta)),
@@ -23,18 +26,25 @@ plot_icr_methylation <- function(mat_cpg_beta, df_patient_groups, icr_id,
             by = join_by("cpg_id"), keep = FALSE, na_matches = "never", 
             relationship = "one-to-one")
   
-
   # Subset the cpg_beta matrix
   sub_mat_cpg_beta = mat_cpg_beta[df$icr_id == icr_id,]
   
   # Reorder rows to genomic location
   sub_mat_cpg_beta <- sub_mat_cpg_beta %>% arrange(df[df$icr_id == icr_id,]$MAPINFO)
+  # ordered_cpg_ids <- df[df$icr_id == icr_id,] %>% arrange(MAPINFO) %>% pull(cpg_id)
   
-  ordered_cpg_ids <- df[df$icr_id == icr_id,] %>% arrange(MAPINFO) %>% pull(cpg_id)
-  
-  
-  
-  
+  # Subset the cpg sites to those around significant cpgs
+  if (nrow(sub_mat_cpg_beta) > 2*max_sig_hwindow+1) {
+    
+    sig_inds <- which(rownames(sub_mat_cpg_beta) %in% sig_cpgs)
+    
+    min_ind <- max(c(min(sig_inds) - max_sig_hwindow, 1))
+    max_ind <- min(c(max(sig_inds) + max_sig_hwindow, nrow(sub_mat_cpg_beta)))
+    
+    sub_mat_cpg_beta <- sub_mat_cpg_beta[min_ind:max_ind,]
+    
+  }
+
   
   # Ordered list of cpg sites (for factor)
   # rownames(sub_mat_cpg_beta)
@@ -45,22 +55,57 @@ plot_icr_methylation <- function(mat_cpg_beta, df_patient_groups, icr_id,
   df_long_cpg_beta <- left_join(x = df_long_cpg_beta, y= df_patient_groups, by = join_by("patient_id"),
             keep = FALSE, na_matches = "never", relationship = "many-to-one")
   
-  df_long_cpg_beta$cpg_id <- factor(df_long_cpg_beta$cpg_id, levels = ordered_cpg_ids, ordered=TRUE)
+  df_long_cpg_beta$cpg_id <- factor(df_long_cpg_beta$cpg_id, levels = rownames(sub_mat_cpg_beta), ordered=TRUE)
   df_long_cpg_beta$group <- factor(df_long_cpg_beta$group, levels = unique(df_long_cpg_beta$group) %>% sort(), ordered=TRUE)
   
   df_summary <- df_long_cpg_beta %>% group_by(cpg_id, group) %>% 
     summarize(beta_mean = mean(value, na.rm = T), beta_sd = sd(value, na.rm = T),
               beta_sd = sd(value, na.rm = T), beta_sem = sd(value, na.rm = T)/sqrt(n()))
+  df_summary$cpg_id_rank = as.numeric(df_summary$cpg_id)
+  df_summary$xmin = df_summary$cpg_id_rank -0.5
+  df_summary$xmax = df_summary$cpg_id_rank +0.5
+  df_summary$back_fill = df_summary$cpg_id_rank %% 2 == 0
+  
+  
+  # Get ICR metadata (closest genes, zinc finger)
+  icr_metadata <- add_metadata_to_imp_sites(icr_id, imp_type = "icr")
+  zinc_finger_str = c("-","+")[(as.numeric(icr_metadata$is_icr_zinc)+1)]
+  icf_conf_str = c("High","Medium", "Low")[icr_metadata$icr_conf]
+  
+  
+  exact_ylim = range(df_summary$beta_mean)
+  padded_ylim  = c(exact_ylim[1] - 0.1*diff(exact_ylim),exact_ylim[2] + 0.1*diff(exact_ylim))
   
   # Plot methylation across cpg sites in ICR
-  ggplot(data = df_summary, aes(x = cpg_id, y = beta_mean, color = group)) +
-    geom_errorbar(aes(ymin = beta_mean - beta_sem, ymax = beta_mean + beta_sem),
-                  position = position_dodge(width=0.6), width = 0.4) +
-    geom_boxplot(position = position_dodge(width=0.6), width = 0.5) + 
-    scale_color_manual(values=c("red", "blue")) + 
-    coord_cartesian(ylim=c(0,1)) +xlab("CpG site") + ylab("Beta Value") + 
-    guides(color=guide_legend(title="Med Diet Score")) +
-    theme_classic()
+  gg <- ggplot(data = df_summary, aes(x = cpg_id, y = beta_mean)) +
+    # geom_rect(aes(fill = back_fill, xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf)) +
+    # geom_linerange (aes(x = cpg_id, ymin = beta_mean - beta_sem,
+    #                  ymax = beta_mean + beta_sem, color = group),
+    #              linewidth = 5, alpha = 0.5, position = position_dodge(width=0.7))+
+    geom_point(aes(color = group), size = 1.5, alpha = 0.5) + 
+    # geom_errorbar(aes(ymin = beta_mean - beta_sem, ymax = beta_mean + beta_sem, color = group),
+                  # position = position_dodge(width=0.7), width = 0.6, linewidth = .2) +
+    geom_text(aes(label = ifelse(df_summary$cpg_id %in% sig_cpgs, "*", "")), 
+              y = padded_ylim[2], size = 4, vjust=1) + 
+    # geom_boxplot(aes(color = group), position = position_dodge(width=0.7), width = 0.7, linewidth = .2) +
+    scale_color_manual(values=c("red", "blue"), labels= c("Low","High"), guide = "none") + 
+    scale_fill_manual(values=c("grey90", "white"), guide = "none") +
+    # scale_x_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = (c(min(df_summary$cpg_id_rank), max(df_summary$cpg_id_rank))), ylim = padded_ylim) + #ylim=c(0,1)
+      xlab(xlab_txt) + ylab("Mean Beta Value") + 
+    # geom_ribbon(aes(fill = ))+
+    # ggtitle(sprintf("%s (%sZF, %s): %s", icr_id, zinc_finger_str,icf_conf_str, 
+    #           icr_metadata$Nearest.Transcript)) +
+    ggtitle(sprintf("%s: %s", icr_id, icr_metadata$Nearest.Transcript)) +
+    theme_classic(base_size = 7) + theme(axis.text.x = element_text(
+      angle = 45,vjust = 1, hjust = 1), plot.title = element_text(size = 7)) 
+  print(gg)
+  save_plot(filename = paste0(output_path, "/", 
+                              sprintf("%s_%s_%sZF", icr_id, icf_conf_str, zinc_finger_str), ".jpg"),
+            plot = gg,base_height = 2, base_width = 2.5)
   
+  
+  
+  ifelse(levels(df_summary$cpg_id) %in% NA, "purple", "black")
   # Export
 }
